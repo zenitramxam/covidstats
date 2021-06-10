b39d493c2160a7267d2fe9647de8cafabd744dc3
Kirk Thomas <dablumaroon@gmail.com>
2021-06-10 14:04:54 -0400

    Creating a branch for Kirk Thomas.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,17 @@
 // https://covid-19-statistics.p.rapidapi.com/reports?date=2020-04-16&q=US%20Alabama&region_name=US&iso=USA&region_province=Alabama&city_name=Autauga", {
 var searchFormEl = document.querySelector('#Search-form');
-
-var searchFormEl = document.querySelector('#search-form');
+var searchInputVal = document.querySelector('#search-input').value;
+var formatInputVal = document.querySelector('#format-input').value;
 
 function searchFormSubmit(event) {
     event.preventDefault();
-    var searchInputVal = document.querySelector('#search-input').value;
-    var formatInputVal = document.querySelector('#format-input').value;
+
 }
 
 
 fetch("https://covid-19-statistics.p.rapidapi.com/reports?" + new URLSearchParams({
     date: '2021-01-04',  // If the parameter holding the date value is called something other than 'date',
-                         // the API will ignore the bogus name and return todays date
+    // the API will ignore the bogus name and return todays date
     q: 'US Texas',
     city_name: 'Austin'
 }), {
@@ -38,15 +37,15 @@ fetch("https://covid-19-statistics.p.rapidapi.com/reports?" + new URLSearchParam
         console.error(err);
     });
 
-    fetch("https://vaccovid-coronavirus-vaccine-and-treatment-tracker.p.rapidapi.com/api/vaccines/get-fda-approved-vaccines", {
-        "method": "GET",
-        "headers": {
-            "x-rapidapi-key": "1eed7d45a6msh68ac90440e53584p1e89eejsnf0b5f25e3f56",
-            "x-rapidapi-host": "vaccovid-coronavirus-vaccine-and-treatment-tracker.p.rapidapi.com"
-        }
-    })
+fetch("https://vaccovid-coronavirus-vaccine-and-treatment-tracker.p.rapidapi.com/api/vaccines/get-fda-approved-vaccines", {
+    "method": "GET",
+    "headers": {
+        "x-rapidapi-key": "1eed7d45a6msh68ac90440e53584p1e89eejsnf0b5f25e3f56",
+        "x-rapidapi-host": "vaccovid-coronavirus-vaccine-and-treatment-tracker.p.rapidapi.com"
+    }
+})
     .then(function (response) {
-    return response.json();
+        return response.json();
     })
     .then(response => {
         console.log(response);
@@ -54,19 +53,19 @@ fetch("https://covid-19-statistics.p.rapidapi.com/reports?" + new URLSearchParam
     .catch(err => {
         console.error(err);
     })
-    fetch("https://vaccovid-coronavirus-vaccine-and-treatment-tracker.p.rapidapi.com/api/vaccines/get-all-fda-approved-treatment", {
-	"method": "GET",
-	"headers": {
-		"x-rapidapi-key": "1eed7d45a6msh68ac90440e53584p1e89eejsnf0b5f25e3f56",
-		"x-rapidapi-host": "vaccovid-coronavirus-vaccine-and-treatment-tracker.p.rapidapi.com"
-	}
+fetch("https://vaccovid-coronavirus-vaccine-and-treatment-tracker.p.rapidapi.com/api/vaccines/get-all-fda-approved-treatment", {
+    "method": "GET",
+    "headers": {
+        "x-rapidapi-key": "1eed7d45a6msh68ac90440e53584p1e89eejsnf0b5f25e3f56",
+        "x-rapidapi-host": "vaccovid-coronavirus-vaccine-and-treatment-tracker.p.rapidapi.com"
+    }
 })
-.then(function (response) {
-    return response.json();
+    .then(function (response) {
+        return response.json();
     })
-.then(response => {
-	console.log(response);
-})
-.catch(err => {
-	console.error(err);
-});
+    .then(response => {
+        console.log(response);
+    })
+    .catch(err => {
+        console.error(err);
+    });
